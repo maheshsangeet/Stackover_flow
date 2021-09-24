bb7842303cbc9d04245876af095a5f6edc4dd0a3
mahesh <maheshsangeet0@gmail.com>
2021-09-24 17:09:31 +0530

    dark theme added

--- js/app.js
+++ js/app.js
@@ -2,22 +2,28 @@
 
 const roundBtn = document.querySelector('.round');
 const toggleBar = document.querySelector('.toggleBar');
-console.log(roundBtn)
-console.log(toggleBar)
-
+const container = document.querySelector('.container');
 
 toggleBar.addEventListener('click',()=> {
     if(roundBtn.classList.contains("roundColor") ) {
         toggleBar.classList.add('darkToggleBar');
-        roundBtn.classList.add('darkrRound');
+        roundBtn.classList.add('darkRound');
+        container.classList.add('darkContainer');
+
         toggleBar.classList.remove('toggleBarColor');
         roundBtn.classList.remove('roundColor');
-        console.log(roundBtn)
-        console.log(toggleBar)  
+       
 
     }else {
-        toggleBar.classList.add('darkToggleBar');
-        roundBtn.classList.add('darkRound');
+        toggleBar.classList.add('toggleBarColor');
+        roundBtn.classList.add('roundColor');
+        roundBtn.classList.add('container');
+
+
+        toggleBar.classList.remove('darkToggleBar');
+        roundBtn.classList.remove('darkRound');
+        container.classList.remove('darkContainer');
+
     }
     
 })
